c258ea69139d07c19c29b26d55a6a5330d714598
Colin <colin.mangas@gmail.com>
2020-05-25 17:54:33 +0200

    wip

---
+++ src/com/company/IOCommand.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IOCommand {
+    private BufferedReader lectureEcran;
+    private PrintStream ecritureEcran;
+    private Socket socket;
+    private boolean isRunning;
+
+
+
+    public IOCommand() throws IOException {
+        this(null);
+    }
+
+    public IOCommand(Socket socket) throws IOException {
+        lectureEcran = null;
+        ecritureEcran = null;
+        this.socket = socket;
+    }
+
+    public void ecrireEcran(String texte) {
+        System.out.println(texte);
+    }
+
+    public String lireEcran() {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Rentrer un texte :");
+
+        String chaine = sc.nextLine();
+        System.out.println(chaine);
+        return chaine;
+    }
+
+    public void ecrireReseau(String texte) throws IOException {
+        ecritureEcran = new PrintStream(socket.getOutputStream(), false);
+        ecritureEcran.print(texte);
+        ecritureEcran.flush();
+    }
+
+    public String lireReseau() throws IOException {
+        lectureEcran = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        return lectureEcran.readLine();
+    }
+
+    public boolean connexion(String ip, int port) {
+        try {
+            socket = new Socket(ip, port);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void deconnexion() throws IOException, InterruptedException {
+        this.isRunning = false;
+        Thread.sleep(100);
+        socket.close();
+    }
+
+    public void setRunning(boolean etat) {
+        isRunning = etat;
+    }
+
+    public void run() {
+        this.isRunning = true;
+        Thread t = new Thread(new Runnable() {
+            public void run(){
+                while(isRunning) {
+                    try {
+                        String retourServ = lireReseau();
+                        ecrireEcran("serv> " + retourServ);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        t.start();
+        if(!isRunning)
+            t.stop();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,46 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        IOCommand command = new IOCommand();
+//        boolean connexion = command.connexion("192.168.1.131", 6002);
+//        boolean connexion = command.connexion("192.168.1.24", 6003);
+//        boolean connexion = command.connexion("192.168.1.131", 6001);
+        boolean connexion = command.connexion("192.168.1.25", 6002);
+
+
+//        int Min = 0;
+//        int Max = 2;
+//
+//        System.out.println(Min + (int)(Math.random() * ((Max - Min) + 1)));
+
+        if(!connexion) return;
+        command.run();
+        String str = "";
+        String choix = "";
+        String speak = "";
+        String nom;
+        String envoieA;
+        boolean commandeConnue = false;
+        Object test = new Object();
+
+
+        command.ecrireEcran("Quel est ton login ?");
+        command.ecrireReseau(command.lireEcran());
+        Thread.sleep(100);
+
+        do {
+            command.ecrireEcran("Que voulez vous faire ?");
+            command.ecrireEcran("1. /serveur\n2. /clientDispo\n3. /speak + login\n4. /all\n5. /quit");
+            choix = command.lireEcran();
+            speak = choix;
+
+        } while(!choix.equals("/quit"));
+        command.ecrireReseau("quit");
+        command.deconnexion();
     }
 }
